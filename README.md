c9422f0fdbe11f6509a31ada50de60034f79d5b8
Gunzi <29294545a@gmail.com>
2019-06-12 00:30:34 +0900

    6/11の進捗

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 参考URL、文献  
 
 * http://www.haun.org/kent/lisp1/2.html
+* [初めての人のためのLISP](https://www.amazon.co.jp/dp/4798119415/ref=cm_sw_r_tw_dp_U_x_FW7.CbVTHZ064)
+
+## 第一の戒律
+
+（仮）いかなる関数を表現するときも最初の質問はすべてnull?にすべし。
 
 ## atom(アトム)
 
@@ -13,12 +18,36 @@
 'atom
 ```
 
-## S(Symbol)式
+## S式
 
-わからねえ…
+lispの構文そのもの  
+「道はnilを生ず、nilはアトムを生じ、アトムはS式を生じ、S式は万物を生ず」  
+
+
+```
+(hogehoge)
+(cdr '(sushi))
+```
 
 ## Car
 
+リストの先頭要素を求める
+
 ## Cdr
 
+先頭の要素を除いたリストを求める
+
 ## Cons
+
+先頭に要素を加えたリストを作る
+
+## Cond
+
+条件分岐
+
+```
+(cond
+  ((hogehoge) fugafuga)
+  ((higehige) mogemoge)
+  (else #f))
+```
--- the_little_schemer.scm
+++ the_little_schemer.scm
@@ -4,5 +4,25 @@
 ;; S式とはリスト構造である
 (quote 'atom)
 ;; carの書き方
-(car '(a b c))
-;;掟の前まで終わった
+(print(car '(a b c)))
+;;((a b c) x y z)のcdr
+(print '((a b c) x y z))
+;;car,cdrは引数として空でないリストをとる
+;;consは引数として、最初にS式をとり２番めに任意のリストをとる
+;;(cons a b) 答えはありません。なぜでしょう
+;;lat?を定義する
+;;S式の最後まで、どのS式もアトムかどうか尋ねる。もしリストが見つかれば直ちに#fを返す
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+;;member?を定義する
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) nil)
+      (else (or (eq? (car lat) a)
+		(member? a (cdr lat)))))))
+;;第1の戒律まで進んだ
