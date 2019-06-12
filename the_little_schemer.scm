7af2e089e6b1780578fe86170ece2b0347bca5af
Gunzi <29294545a@gmail.com>
2019-06-13 00:20:50 +0900

    6/12の進捗

--- the_little_schemer.scm
+++ the_little_schemer.scm
@@ -1,17 +1,6 @@
 (define atom?
   (lambda (x)
   (and (not (pair? x)) (not (null? x)))))
-;; S式とはリスト構造である
-(quote 'atom)
-;; carの書き方
-(print(car '(a b c)))
-;;((a b c) x y z)のcdr
-(print '((a b c) x y z))
-;;car,cdrは引数として空でないリストをとる
-;;consは引数として、最初にS式をとり２番めに任意のリストをとる
-;;(cons a b) 答えはありません。なぜでしょう
-;;lat?を定義する
-;;S式の最後まで、どのS式もアトムかどうか尋ねる。もしリストが見つかれば直ちに#fを返す
 (define lat?
   (lambda (l)
     (cond
@@ -25,4 +14,11 @@
       ((null? lat) nil)
       (else (or (eq? (car lat) a)
 		(member? a (cdr lat)))))))
-;;第1の戒律まで進んだ
+;;remberを定義する
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat)
+	    (rember a (cdr lat)))))))
