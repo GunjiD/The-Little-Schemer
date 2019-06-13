053c1983e68ca8d7b9651fa19f8a56c079702b2e
Gunzi <29294545a@gmail.com>
2019-06-14 00:25:43 +0900

    6/13の進捗

--- the_little_schemer.scm
+++ the_little_schemer.scm
@@ -22,3 +22,56 @@
       ((eq? (car lat) a) (cdr lat))
       (else (cons (car lat)
 	    (rember a (cdr lat)))))))
+;;firstsを定義する
+;;なぜfirsts (cdr l)なのか -> 一度に一つのS式しか見ないため。残りを見るには cdr で先頭以外の要素を取り出す必要がある
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) quote ())
+      (else (cons (car (car l))
+		  (firsts (cdr l)))))))
+;;insertRを定義する
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+	      ((eq? (car lat) old)
+	       (cons old
+		(cons new (cdr lat))))
+	      (else (cons (car lat)
+			  (insertR new old
+				   (cdr lat)))))))))
+;;insertLを定義する
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+	      ((eq? (car lat) old)
+	       (cons new lat))
+	      (else (cons (car lat)
+		    (insertL new old
+			     (cdr lat)))))))))
+;;substを定義する lat の中の最初に出てきた old を new に置き換える
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+	      ((eq? (car lat) old)
+	       (cons new (cdr lat)))
+	      (else (cons (car lat)
+			  (subst new old
+				 (cdr lat)))))))))
+;;subst2
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+	      ((or (eq? (car lat) o1) (eq? (car lat) o2))
+	       (cons new (cdr lat)))
+	      (else (cons (car lat)
+			  (subst2 new o1 o2
+			  (cdr lat2)))))))))
